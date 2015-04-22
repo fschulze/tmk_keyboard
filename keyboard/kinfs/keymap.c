f2a692008ff822ade8fcd733fbd6710a2e90f833
Florian Schulze <florian.schulze@gmx.net>
2015-04-22 20:26:49 +0200

    Tweaks after playing a bit with things.

--- keyboard/kinfs/keymap.c
+++ keyboard/kinfs/keymap.c
@@ -40,6 +40,78 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                MINS,BSLS,QUOT,RSFT),
 
     [1] =
+    KEYMAP(F6  ,F3  ,ESC ,F9  ,F12 ,PAUS,           \
+           F8  ,F5  ,F2  ,F11 ,SLCK,                \
+           F7  ,F4  ,F1  ,F10 ,PSCR,                \
+           5   ,T   ,G   ,B   ,RGHT,FN0 ,LGUI,      \
+           4   ,R   ,F   ,V   ,          ESC ,FN0, \
+           3   ,E   ,D   ,C   ,LEFT,LALT,BSPC,      \
+           2   ,W   ,S   ,X   ,NUBS,     RGUI,RALT, \
+           1   ,Q   ,A   ,Z   ,GRV ,LCTL,ENT ,      \
+           EQL ,TAB ,LCTL,LSFT,          SPC ,RCTL , \
+                          UP  ,6   ,Y   ,H   ,N   , \
+                               7   ,U   ,J   ,M   , \
+                          DOWN,8   ,I   ,K   ,COMM, \
+                                                    \
+                          LBRC,9   ,O   ,L   ,DOT , \
+                          RBRC,0   ,P   ,SCLN,SLSH, \
+                               MINS,BSLS,QUOT,RSFT),
+
+    [2] =
+    KEYMAP(F6  ,F3  ,ESC ,F9  ,F12 ,PAUS,           \
+           F8  ,F5  ,F2  ,F11 ,SLCK,                \
+           F7  ,F4  ,F1  ,F10 ,PSCR,                \
+           5   ,B   ,G   ,V   ,RGHT,FN0 ,LGUI,      \
+           4   ,W   ,T   ,C   ,          ESC ,FN0, \
+           3   ,R   ,H   ,M   ,LEFT,LALT,BSPC,      \
+           2   ,D   ,S   ,X   ,NUBS,     RGUI,RALT, \
+           1   ,Q   ,A   ,Z   ,GRV ,LCTL,ENT ,      \
+           EQL ,TAB ,LCTL,LSFT,          SPC ,RCTL , \
+                          UP  ,6   ,J   ,Y   ,K   , \
+                               7   ,F   ,N   ,L   , \
+                          DOWN,8   ,U   ,E   ,COMM, \
+                                                    \
+                          LBRC,9   ,P   ,O   ,DOT , \
+                          RBRC,0   ,SCLN,I   ,SLSH, \
+                               MINS,BSLS,QUOT,RSFT),
+
+    [3] =
+    KEYMAP(F6  ,F3  ,ESC ,F9  ,F12 ,PAUS,           \
+           F8  ,F5  ,F2  ,F11 ,SLCK,                \
+           F7  ,F4  ,F1  ,F10 ,PSCR,                \
+           5   ,B   ,G   ,V   ,RGHT,FN0 ,LGUI,      \
+           4   ,W   ,T   ,C   ,          ESC ,FN0, \
+           3   ,R   ,H   ,M   ,LEFT,LALT,BSPC,      \
+           2   ,D   ,S   ,X   ,NUBS,     RGUI,RALT, \
+           1   ,Q   ,A   ,Z   ,GRV ,LCTL,ENT ,      \
+           EQL ,TAB ,LCTL,LSFT,          SPC ,RCTL , \
+                          UP  ,6   ,J   ,Y   ,K   , \
+                               7   ,F   ,N   ,L   , \
+                          DOWN,8   ,U   ,E   ,COMM, \
+                                                    \
+                          LBRC,9   ,P   ,O   ,DOT , \
+                          RBRC,0   ,SCLN,I   ,SLSH, \
+                               MINS,BSLS,QUOT,RSFT),
+
+    [4] =
+    KEYMAP(F6  ,F3  ,ESC ,F9  ,F12 ,PAUS,           \
+           F8  ,F5  ,F2  ,F11 ,SLCK,                \
+           F7  ,F4  ,F1  ,F10 ,PSCR,                \
+           5   ,B   ,G   ,V   ,RGHT,FN0 ,LGUI,      \
+           4   ,W   ,T   ,C   ,          ESC ,FN0, \
+           3   ,R   ,H   ,M   ,LEFT,LALT,BSPC,      \
+           2   ,D   ,S   ,X   ,NUBS,     RGUI,RALT, \
+           1   ,Q   ,A   ,Z   ,GRV ,LCTL,ENT ,      \
+           EQL ,TAB ,LCTL,LSFT,          SPC ,RCTL , \
+                          UP  ,6   ,J   ,Y   ,K   , \
+                               7   ,F   ,N   ,L   , \
+                          DOWN,8   ,U   ,E   ,COMM, \
+                                                    \
+                          LBRC,9   ,P   ,O   ,DOT , \
+                          RBRC,0   ,SCLN,I   ,SLSH, \
+                               MINS,BSLS,QUOT,RSFT),
+
+    [5] =
     KEYMAP(TRNS,TRNS,PWR ,TRNS,TRNS,TRNS,           \
            TRNS,TRNS,TRNS,TRNS,TRNS,                \
            TRNS,TRNS,TRNS,TRNS,TRNS,                \
@@ -59,6 +131,6 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    ACTION_LAYER_MOMENTARY(1),
+    ACTION_LAYER_MOMENTARY(5),
 };
 
