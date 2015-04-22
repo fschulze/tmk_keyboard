3c8f757ceb7f854cbf027b5974085c1254d7e0b5
Florian Schulze <florian.schulze@gmx.net>
2015-04-22 23:24:56 +0200

    Switch to teensy 2.0.

--- keyboard/kinfs/led.c
+++ keyboard/kinfs/led.c
@@ -23,32 +23,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* HHKB has no LEDs */
 void led_set(uint8_t usb_led)
 {
-    if (usb_led&(1<<USB_LED_COMPOSE)) {
-        DDRB  |=  (1<<5);
-        PORTB &= ~(1<<5);
-    } else {
-        DDRB  |=  (1<<5);
-        PORTB |=  (1<<5);
-    }
-    if (usb_led&(1<<USB_LED_NUM_LOCK)) {
-        DDRB  |=  (1<<7);
-        PORTB &= ~(1<<7);
-    } else {
-        DDRB  |=  (1<<7);
-        PORTB |=  (1<<7);
-    }
-    if (usb_led&(1<<USB_LED_SCROLL_LOCK)) {
-        DDRB  |=  (1<<6);
-        PORTB &= ~(1<<6);
-    } else {
-        DDRB  |=  (1<<6);
-        PORTB |=  (1<<6);
-    }
-    if (usb_led&(1<<USB_LED_KANA)) {
+    if (usb_led&(1<<USB_LED_CAPS_LOCK)) {
         DDRC  |=  (1<<7);
         PORTC &= ~(1<<7);
     } else {
         DDRC  |=  (1<<7);
         PORTC |=  (1<<7);
     }
+    if (usb_led&(1<<USB_LED_NUM_LOCK)) {
+        DDRB  |=  (1<<6);
+        PORTB &= ~(1<<6);
+    } else {
+        DDRB  |=  (1<<6);
+        PORTB |=  (1<<6);
+    }
+    if (usb_led&(1<<USB_LED_SCROLL_LOCK)) {
+        DDRB  |=  (1<<5);
+        PORTB &= ~(1<<5);
+    } else {
+        DDRB  |=  (1<<5);
+        PORTB |=  (1<<5);
+    }
+    if (usb_led&(1<<USB_LED_COMPOSE)) {
+        DDRD  |=  (1<<6);
+        PORTD |=  (1<<6);
+    } else {
+        DDRD  |=  (1<<6);
+        PORTD &=  ~(1<<6);
+    }
+    DDRD  |=  (1<<7);
+    PORTD &= ~(1<<7);
 }
